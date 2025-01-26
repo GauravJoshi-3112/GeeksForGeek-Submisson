cf1323def5e8ff9b34faa547bac24e1303c8b014
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-01-27 00:38:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+
+class GFG {
+    public static void printList(Node node) {
+        Node tmp = node;
+        int c1 = 0, c2 = 0;
+        if (tmp != null) {
+            while (tmp.next != null) {
+                c1++;
+                tmp = tmp.next;
+            }
+            while (tmp.prev != null) {
+                c2++;
+                tmp = tmp.prev;
+            }
+            if (c1 != c2) {
+                System.out.println("-1");
+                return;
+            }
+        }
+        while (tmp != null) {
+            System.out.print(tmp.data + " ");
+            tmp = tmp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            Node ans = obj.constructDLL(arr);
+            printList(ans);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    Node constructDLL(int arr[]) {
+        // arr --> DLL 
+        Node head = null, curr = null, tail = null;
+        if (arr.length == 0) return head; // Since no element exist Null should be the head for empty Linked Lists
+        for(int x : arr) {
+            if (head == null) { // First Element of the array
+                head = new Node(x);
+                tail = head;
+                curr = head;
+                continue;
+            } 
+            curr = new Node(x);
+            curr.prev = tail;
+            tail.next = curr;
+            tail = tail.next;
+        }
+        return head;
+    
+    }
+}
+    
