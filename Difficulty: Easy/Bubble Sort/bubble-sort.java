12979b2cb35fbf3b98e66b01d135eeb689d980b5
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-03-04 02:02:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.java
+++ Difficulty: Easy/Bubble Sort/bubble-sort.java
@@ -10,26 +10,21 @@ import java.util.*;
 // User function Template for Java
 
 class Solution {
+    public static void bubbleSort(int arr[],int high) {
+        if (high <= 0) return;
+        for(int i=0; i<high; i++) {
+            if (arr[i] > arr[i+1]) {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        bubbleSort(arr,high-1);
+    }
     // Function to sort the array using bubble sort algorithm.
     public static void bubbleSort(int arr[]) {
-        // Bubble Sort Algorith Swaps Adjacent Element to make them in correct position for n-1 iterations
-        int n = arr.length;
-        // N-1 Iteration (If N-1 Element's are at Correct Position then Nth will be in Correct pos)
-        for(int i=0; i<n-1; i++) {
-            // For Every ITeration Swap Adjacent 
-            boolean isSwapped = false; // Optimization for Already Sorted Array
-            for(int j=0; j<n-i-1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    // Need to Perform Swapping
-                    isSwapped = true;
-                    // Swapping 
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-            if (!isSwapped) break; // Since No Swapping happens in this Iteration Array is Sorted
-        }
+        // code here
+        bubbleSort(arr,arr.length-1);
     }
 }
 
