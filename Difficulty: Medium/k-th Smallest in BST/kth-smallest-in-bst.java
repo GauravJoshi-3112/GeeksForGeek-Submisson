18e283dffbd6dd776db219fe32fb978473f3b6f7
Gaurav Joshi <128222596+GauravJoshi-3112@users.noreply.github.com>
2025-07-09 12:29:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.java
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.java
@@ -14,11 +14,17 @@ class Solution {
     int index = 1;
     // Return the Kth smallest element in the given BST
     public int kthSmallest(Node root, int k) {
-        if (root == null) return -1;
+        // Write your code here
+        if (root == null) return -1; // The Edge Case Outside Index 
+        
         int left = kthSmallest(root.left, k);
-        if (left != -1) return left;
+        if (left != -1) return left; // Index Exsits at Left Index
+        
+        // Root is the Index
         if (index == k) return root.data;
-        else index+=1;
+        
+        // Increament Index for Right Traversal
+        index++;
         return kthSmallest(root.right, k);
     }
 }
